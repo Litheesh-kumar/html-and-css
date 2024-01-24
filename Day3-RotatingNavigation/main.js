5659da7ce18d0a8c0c591cd2177fe3d1358c5b8d
Litheesh-kumar <kumarlitheesh8@gmail.com>
2024-01-24 08:28:39 +0530

    rotation navigation

---
+++ Day3-RotatingNavigation/main.js
@@ -0,0 +1,6 @@
+const openbtn = document.querySelector('#open');
+const closebtn = document.querySelector('#close');
+const container = document.querySelector('.container');
+
+openbtn.addEventListener('click',()=> container.classList.add('show-nav'));
+closebtn.addEventListener('click',()=> container.classList.remove('show-nav'));
